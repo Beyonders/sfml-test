aa645097984fc158e6c58e5e67562643cd1230c4
Brent Patterson <bcpatterson@xactware.com>
2014-11-06 17:01:52 -0700

    More work on sprite manager.

--- SpriteManager.cpp
+++ SpriteManager.cpp
@@ -9,6 +9,93 @@ bool _initialized = false;
 std::map<std::string, sf::Sprite> ParentSprites; //To reduce the memory usage
 std::map<std::string, sf::Sprite> Sprites;
 
+
+
+float BBSprite::GetNextFrameLength()
+{
+	if (_baseSprite.FrameLength[_currentFrame].size() == 1)
+	{
+		return _baseSprite.FrameLength[_currentFrame][0] / 1000.0f;
+	}
+	else if (_baseSprite.FrameLength[_currentFrame].size() == 2)
+	{
+		int min = _baseSprite.FrameLength[_currentFrame][0];
+		int max = _baseSprite.FrameLength[_currentFrame][1];
+
+		return (rand() % max + min) / 1000.0f;
+	}
+	return 0;
+}
+
+BBSprite::BBSprite(BaseSprite& baseSprite)
+{
+		_baseSprite = baseSprite;
+		_currentFrame = 0;
+		_animated = _baseSprite.Frames.size() > 1;
+		if (_animated)
+		{
+			_frameTimer = GetNextFrameLength();
+		}
+}
+
+void BBSprite::Update(float time)
+{
+	if (!_animated)
+	{
+		return;
+	}
+	_frameTimer -= time;
+	if (_frameTimer <= 0)
+	{
+		//Advance to next frame
+		_currentFrame++;
+		if (_currentFrame >= _baseSprite.Frames.size())
+		{
+			_currentFrame = 0;
+		}
+		_frameTimer = GetNextFrameLength();
+	}
+}
+
+void BBSprite::Draw(float x, float y)
+{
+	sf::Color color = sf::Color(255,255,255,255);
+	Draw(x, y, 1, 1, color, 0.0f);
+}
+
+void BBSprite::Draw(float x, float y, float scale)
+{
+	sf::Color color = sf::Color(255,255,255,255);
+	Draw(x, y, scale, scale, color, 0.0f);
+}
+
+void BBSprite::Draw(float x, float y, float scaleX, float scaleY)
+{
+	sf::Color color = sf::Color(255,255,255,255);
+	Draw(x, y, scaleX, scaleY, color, 0.0f);
+}
+
+void BBSprite::Draw(float x, float y, float scaleX, float scaleY, char alpha)
+{
+	sf::Color color = sf::Color(255,255,255,alpha);
+	Draw(x, y, 1, 1, color, 0.0f);
+}
+
+void BBSprite::Draw(float x, float y, float scaleX, float scaleY, sf::Color& color)
+{
+	Draw(x, y, 1, 1, color, 0.0f);
+}
+
+void BBSprite::Draw(float x, float y, float scaleX, float scaleY, sf::Color& color, float angle)
+{
+	sf::Sprite frame = _baseSprite.Frames[_currentFrame];
+	frame.setRotation(angle);
+	frame.setPosition(x, y);
+	frame.setScale(scaleX, scaleY);
+	frame.setColor(color);
+	//frame->draw();
+}
+
 bool Initialize(std::string path)
 {
 	if (_initialized) //already initalized
@@ -26,6 +113,7 @@ bool Initialize(std::string path)
 			{
 				continue;
 			}
+			
 		}
 		spriteListFile.close();
 		_initialized = true;
--- SpriteManager.h
+++ SpriteManager.h
@@ -1,6 +1,44 @@
 #include <SFML/Graphics.hpp>
 #include <map>
 
+enum StretchMode { STRETCH, REPEAT };
+
+class BaseSprite
+{
+private:
+	StretchMode _stretchMode;
+
+public:
+	std::vector<sf::Sprite> Frames;
+	std::vector<std::vector<int>> FrameLength;
+	std::string Name;
+
+	bool LoadSprite(sf::Sprite parentSprite, std::string name, int frames, std::string frameStrings[]);
+};
+
+class BBSprite
+{
+private:
+	BaseSprite _baseSprite;
+	int _currentFrame;
+	float _frameTimer;
+	bool _animated;
+	float GetNextFrameLength();
+
+public:
+	float Width;
+	float Height;
+
+	BBSprite(BaseSprite& baseSprite);
+	void Update(float time);
+	void Draw(float x, float y);
+	void Draw(float x, float y, float scale);
+	void Draw(float x, float y, float scaleX, float scaleY);
+	void Draw(float x, float y, float scaleX, float scaleY, char alpha);
+	void Draw(float x, float y, float scaleX, float scaleY, sf::Color& color);
+	void Draw(float x, float y, float scaleX, float scaleY, sf::Color& color, float angle);
+};
+
 extern bool _initialized;
 
 extern std::map<std::string, sf::Sprite> ParentSprites; //To reduce the memory usage
